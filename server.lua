049f765811c664d1b61f8f788dae0980610a59f7
VulgoMr1221 <130130859+VulgoMr1221@users.noreply.github.com>
2023-04-07 02:50:25 -0400

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,17 @@
+﻿addEvent("giveCashMoney", true)
+addEventHandler("giveCashMoney", root,
+    function(money)
+    	givePlayerMoney ( source, money )
+    end
+)
+
+addEvent("takeMoneyBalance", true)
+addEventHandler("takeMoneyBalance", root,
+    function(money)
+    	takePlayerMoney ( source, money )
+    end
+)
+
+function msgBox (player, scrtype, message, type)
+    triggerClientEvent(player, "Notify", player, type, message)
+end
